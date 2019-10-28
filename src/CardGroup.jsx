ace80b673b7fa88a44e8e44854c2411b617f1c85
Ybrayym <ybrayymjan90@gmail.com>
2019-10-28 16:04:47 -0400

    react cards

---
+++ src/CardGroup.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./CardGroup.css";
+
+/*
+ * CardGroup
+ * This component takes Card components as children and renders them.
+ * Example usage:
+ *
+ *      <CardGroup>
+ *          <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
+ *      </CardGroup>
+ *
+ * The above would render a card group with a single child in it.
+ *
+ */
+const CardGroup = (props) => (
+    <div className="cardGroup">
+        {props.children}
+    </div>
+);
+
+export default CardGroup
